84edac0126772d2cc6420a8fd0adf4d3a604a00e
agreyson <amgreyson@gmail.com>
2019-08-22 08:26:48 -0700

    Initial Commit

---
+++ GuessMyNumber/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GuessMyNumber
+//
+//  Created by agreyson on 8/22/19.
+//  Copyright © 2019 agreyson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
